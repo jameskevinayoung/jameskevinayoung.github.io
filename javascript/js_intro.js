55e108f829220f31159130384f6f19796a3a3d73
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-08-08 11:20:39 -0500

    added JS folder, js_intro.html, external_js, inline_js.html, js_intro.js

---
+++ javascript/js_intro.js
@@ -0,0 +1,22 @@
+"use strict";
+var myName = prompt("What is your name?");
+
+function birthday(name) { //named function//
+    var birthdayMessage = "Happy Birthday! " + name + "!!!";
+    console.log(birthdayMessage);
+}
+
+
+// var birthdayMessage - function (input) { this an anonymous function
+//
+// }
+
+
+birthday("James");
+birthday("Taylor");
+birthday("Shawna");
+
+
+
+
+
