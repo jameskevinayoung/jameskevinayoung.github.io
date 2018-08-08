ccaf123a39d023823c6ca93ab656b705f52cfbf4
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-08-08 17:14:59 -0500

    updated functions exercises

--- javascript/js_intro.js
+++ javascript/js_intro.js
@@ -18,5 +18,18 @@ birthday("Shawna");
 
 
 
+function yell(input) {
+    return input.toUpperCase()
+}
 
+// 4 pieces fo a function
+// 1) "function", 2) the name of the function, 3) parameters in parentheses, body
 
+// Step 2 is to call the function
+alert(yell("WHAT UP MOTHERFUCKA"))
+
+// EVERY function has a return value (no return value/value is undefined
+
+fucntion add(a, b) {
+    return a + b;
+}
